74f9d22caf17bb761f8a439be4022c2ac86f5591
George Politis <gp@jitsi.org>
2023-06-28 12:30:37 +0200

    Formatting.

--- jira2ics.py
+++ jira2ics.py
@@ -4,32 +4,35 @@ import os
 from dotenv import load_dotenv
 from atlassian import Jira
 from icalendar import Calendar, Todo
-import dateutil.parser 
+import dateutil.parser
 import argparse
 import utils
 
-#import logging
-#logging.basicConfig()
-#logging.getLogger().setLevel(logging.DEBUG)
+# import logging
+# logging.basicConfig()
+# logging.getLogger().setLevel(logging.DEBUG)
+
 
 def make_todo(issue):
     todo = Todo()
-    todo.add('uid', 'id-{}'.format(issue['id']))
-    todo.add('url', issue['self'])
-    fields = issue['fields']
-    todo.add('summary', '[{}] {}'.format(issue['key'], fields['summary']))
-    todo.add('description', fields['description'])
-    todo.add('created', dateutil.parser.isoparse(fields['created']))
-    todo.add('last-modified', dateutil.parser.isoparse(fields['updated']))
-    todo.add('status', 'NEEDS-ACTION')
-    todo.add('categories', ['Work'])
+    todo.add("uid", "id-{}".format(issue["id"]))
+    todo.add("url", issue["self"])
+    fields = issue["fields"]
+    todo.add("summary", "[{}] {}".format(issue["key"], fields["summary"]))
+    todo.add("description", fields["description"])
+    todo.add("created", dateutil.parser.isoparse(fields["created"]))
+    todo.add("last-modified", dateutil.parser.isoparse(fields["updated"]))
+    todo.add("status", "NEEDS-ACTION")
+    todo.add("categories", ["Work"])
     return todo
-        
+
+
 def make_calendar(json):
     cal = Calendar()
-    for issue in json['issues']:
+    for issue in json["issues"]:
         cal.add_component(make_todo(issue))
     return cal
+
 
 def jira2ics(args):
     cookies = utils.parse_chrome_cookie_file(args.chrome_cookies)
@@ -47,10 +50,18 @@ def jira2ics(args):
 
 load_dotenv()
 
-parser = argparse.ArgumentParser(description='Convert Jira issues to iCal format.')
-parser.add_argument('--chrome-cookies', type=argparse.FileType('r'), default="chrome_cookies.txt")
-parser.add_argument('--outfile', type=argparse.FileType('wb'), default=os.getenv("outfile") or "-")
-parser.add_argument('--jql', default=os.getenv("jql") or "resolution = Unresolved AND assignee in (currentUser())")
-parser.add_argument('url', nargs='?', default=os.getenv("url"))
+parser = argparse.ArgumentParser(description="Convert Jira issues to iCal format.")
+parser.add_argument(
+    "--chrome-cookies", type=argparse.FileType("r"), default="chrome_cookies.txt"
+)
+parser.add_argument(
+    "--outfile", type=argparse.FileType("wb"), default=os.getenv("outfile") or "-"
+)
+parser.add_argument(
+    "--jql",
+    default=os.getenv("jql")
+    or "resolution = Unresolved AND assignee in (currentUser())",
+)
+parser.add_argument("url", nargs="?", default=os.getenv("url"))
 
 jira2ics(parser.parse_args())
